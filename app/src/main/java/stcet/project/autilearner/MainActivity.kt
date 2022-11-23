28832cba670c254e381acf87571f87c89490aecb
PrajeetGuha <prajeetguha@gmail.com>
2022-11-23 21:04:51 +0530

    signup_page
    mainpage done, signup page done

--- app/src/main/java/stcet/project/autilearner/MainActivity.kt
+++ app/src/main/java/stcet/project/autilearner/MainActivity.kt
@@ -1,11 +1,18 @@
 package stcet.project.autilearner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_letsBegin).setOnClickListener {
+            val intentSignup = Intent(this,SignUpActivity::class.java)
+            startActivity(intentSignup)
+        }
     }
 }
