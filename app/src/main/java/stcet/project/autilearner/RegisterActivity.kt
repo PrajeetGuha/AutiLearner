bc1b253fd93a0be96f544e340af73f03e31eac7e
PrajeetGuha <prajeetguha@gmail.com>
2022-12-16 19:25:47 +0530

    minor bug fizes
    tweaking some field errors

--- app/src/main/java/stcet/project/autilearner/RegisterActivity.kt
+++ app/src/main/java/stcet/project/autilearner/RegisterActivity.kt
@@ -1,10 +1,14 @@
 package stcet.project.autilearner
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import stcet.project.autilearner.helper.Identity
@@ -30,45 +34,62 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun checkAllFields(){
         val validate = Validation()
-        val emaillabel = findViewById<TextInputLayout>(R.id.emailLabel)
-        val emailview = findViewById<TextInputEditText>(R.id.emailField)
-        val passwordlabel = findViewById<TextInputLayout>(R.id.passwordLabel)
-        val passwordview = findViewById<TextInputEditText>(R.id.passwordField)
-        val reenteredpasswordlabel = findViewById<TextInputLayout>(R.id.confirmPasswordLabel)
-        val reenteredpasswordview = findViewById<TextInputEditText>(R.id.confirmPasswordField)
+        val emailLabel = findViewById<TextInputLayout>(R.id.emailLabel)
+        val emailView = findViewById<TextInputEditText>(R.id.emailField)
+        val passwordLabel = findViewById<TextInputLayout>(R.id.passwordLabel)
+        val passwordView = findViewById<TextInputEditText>(R.id.passwordField)
+        val reenteredPasswordLabel = findViewById<TextInputLayout>(R.id.confirmPasswordLabel)
+        val reenteredPasswordView = findViewById<TextInputEditText>(R.id.confirmPasswordField)
         var checked = 0
 
-        when(validate.validateEmail(emailview.text.toString())){
+        when(validate.validateEmail(emailView.text.toString())){
 
-            1 -> emaillabel.setError("Expected an email address")
-            2 -> emaillabel.setError("Expected a valid email address")
+            1 -> emailLabel.error = "Expected an email address"
+            2 -> emailLabel.error = "Expected a valid email address"
             3 -> {
-                emaillabel.setError(null)
+                emailLabel.error = null
                 checked += 1
             }
         }
 
-        when(validate.validatePassword(passwordview.text.toString())){
+        when(validate.validatePassword(passwordView.text.toString())){
 
-            1 -> passwordlabel.setError("Expected a password")
-            2 -> passwordlabel.setError("8 characters, 1 uppercase, 1 lowercase, 1 digit and 1 special character atleast")
+            1 -> passwordLabel.error = "Expected a password"
+            2 -> passwordLabel.error = "8 characters, 1 uppercase, 1 lowercase, 1 digit and 1 special character atleast"
             3 -> {
-                passwordlabel.setError(null)
+                passwordLabel.error = null
                 checked += 1
             }
         }
 
-        if(!validate.checkPasswordMatches(passwordview.text.toString(), reenteredpasswordview.text.toString()))
-            reenteredpasswordlabel.setError("Re-entered password don't match")
+        if(!validate.checkPasswordMatches(passwordView.text.toString(), reenteredPasswordView.text.toString()))
+            reenteredPasswordLabel.error = "Re-entered password don't match"
+
         else{
-            reenteredpasswordlabel.setError(null)
+            reenteredPasswordLabel.error = null
             checked += 1
         }
+
+        passwordView.addTextChangedListener( object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                passwordLabel.error = null
+            }
+        })
+
+        reenteredPasswordView.addTextChangedListener( object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                reenteredPasswordLabel.error = null
+            }
+        })
 
         if ( checked == 3 ){
             Log.d("VALIDATION","All data is validated")
             val register = Identity()
-            register.registerUser(this,emailview.text.toString(), passwordview.text.toString())
+            register.registerUser(this,emailView.text.toString(), passwordView.text.toString())
         }
     }
 }
