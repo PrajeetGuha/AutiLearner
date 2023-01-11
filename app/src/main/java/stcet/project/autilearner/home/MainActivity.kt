6f0e6c1447e6667f2a3d3eee54254b90304eb389
Triasa Munshi <triyasam989@gmail.com>
2023-01-11 12:08:42 +0530

    college_minor_fixes

--- app/src/main/java/stcet/project/autilearner/home/MainActivity.kt
+++ app/src/main/java/stcet/project/autilearner/home/MainActivity.kt
@@ -13,5 +13,6 @@ class MainActivity : AppCompatActivity() {
 
         val auth = AuthO()
 
+
     }
 }
--- app/src/main/java/stcet/project/autilearner/home/MainFragment.kt
+++ app/src/main/java/stcet/project/autilearner/home/MainFragment.kt
@@ -1,22 +1,16 @@
 package stcet.project.autilearner.home
 
-import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import stcet.project.autilearner.R
-import stcet.project.autilearner.adapter.CategoriesAdapter
-import stcet.project.autilearner.dataloader.FirestoreDataLoader
-import stcet.project.autilearner.model.Category
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.fragment_main) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val recyclerView = view.findViewById<RecyclerView>(R.id.main_recyclerview)
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        val recyclerView = view.findViewById<RecyclerView>(R.id.main_recyclerview)
 //        val database = FirestoreDataLoader()
-        recyclerView.adapter = CategoriesAdapter(mutableListOf<Category>())
-    }
+//        recyclerView.adapter = TestAdapter(mutableListOf<Category>())
+//        recyclerView.setHasFixedSize(true)
+//    }
 }
