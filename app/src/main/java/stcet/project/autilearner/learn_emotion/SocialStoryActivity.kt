5fa5d89ceb00f873f23ef192ca207628a50608c4
Triasa Munshi <triyasam989@gmail.com>
2023-05-21 21:50:19 +0530

    Changed files

--- app/src/main/java/stcet/project/autilearner/learn_emotion/SocialStoryActivity.kt
+++ app/src/main/java/stcet/project/autilearner/learn_emotion/SocialStoryActivity.kt
@@ -2,43 +2,18 @@ package stcet.project.autilearner.learn_emotion
 
 import android.content.Intent
 import android.os.Bundle
-
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.FrameLayout.LayoutParams
-import android.widget.ImageSwitcher
-
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
 import stcet.project.autilearner.R
 
 
 class SocialStoryActivity : AppCompatActivity() {
 
-    private lateinit var imageSwitcher: ImageSwitcher
-    private lateinit var btnPrevious: Button
-    private lateinit var btnNext: Button
-
-    private var imageUrls: MutableList<String> = mutableListOf()
-    private var currentIndex = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_social_story)
-        imageSwitcher = findViewById(R.id.imageSwitcher)
-        btnPrevious = findViewById(R.id.prev)
-        btnNext = findViewById(R.id.next)
-
-        imageSwitcher.setFactory {
-            ImageView(this@SocialStoryActivity).apply {
-                scaleType = ImageView.ScaleType.FIT_CENTER
-                layoutParams = FrameLayout.LayoutParams(
-                    FrameLayout.LayoutParams.MATCH_PARENT,
-                    FrameLayout.LayoutParams.MATCH_PARENT
-                )
-            }
-        }
     }
+
 
     override fun onStart() {
         super.onStart()
@@ -46,6 +21,5 @@ class SocialStoryActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         val intent = Intent(this, LearnEmotionActivity::class.java)
-        startActivity(intent)
     }
 }
